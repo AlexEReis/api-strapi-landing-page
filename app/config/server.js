bb732364e7069948b01e4f3b0fc5cc778b986c64
AlexEReis <alexeloyreis@gmail.com>
2022-05-27 12:10:06 -0300

    frist commit

---
+++ app/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '2c3158101558602cd5ccac75b16f5860'),
+    },
+  },
+});
---
+++ app/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'cb948470-5eb3-4dbb-aad4-00f1b8eaf64b'
+};
